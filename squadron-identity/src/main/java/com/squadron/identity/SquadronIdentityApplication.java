0fd4898c875ab413a94ae2efec9bda2cbee7bf56
cccs-paul <paul.lessard@cyber.gc.ca>
2026-03-28 17:23:38 -0400

    WIP docker

--- squadron-identity/src/main/java/com/squadron/identity/SquadronIdentityApplication.java
+++ squadron-identity/src/main/java/com/squadron/identity/SquadronIdentityApplication.java
@@ -2,9 +2,18 @@ package com.squadron.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.squadron.identity", "com.squadron.common"})
+@ComponentScan(
+    basePackages = {"com.squadron.identity", "com.squadron.common"},
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = com.squadron.common.exception.GlobalExceptionHandler.class
+    )
+)
 @EnableJpaRepositories
 public class SquadronIdentityApplication {
 
